5e8a4b5f0c4a9c33c3f3957d303723e52035e52d
wwhenxuan <wwhenxuan@gmail.com>
2026-03-02 11:20:19 +0800

    whenxuan: update the __all__ for the init of simulator

--- s2generator/simulator/__init__.py
+++ s2generator/simulator/__init__.py
@@ -6,6 +6,8 @@ Created on 2026/02/13 13:04:42
 @url: https://github.com/wwhenxuan/S2Generator
 """
 
+__all__ = ["ARIMASimulator", "WienerFilterSimulator"]
+
 from .arima import ARIMASimulator
 
 from .wiener_filter import WienerFilterSimulator
