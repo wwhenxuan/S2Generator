f0685e938fed1aba7a51986c9822bb58c8d1e976
wwhenxuan <wwhenxuan@gmail.com>
2026-03-05 01:20:47 +0800

    whenxuan: update the unit test for data augmentation

--- s2generator/augmentation/__init__.py
+++ s2generator/augmentation/__init__.py
@@ -6,10 +6,13 @@ Created on 2026/03/02 12:15:45
 @url: https://github.com/wwhenxuan/S2Generator
 """
 
-__all__ = ["frequency_perturbation", "amplitude_modulation"]
-
-# Import the frequency perturbation function
-from .frequency_perturbation import frequency_perturbation
+__all__ = ["amplitude_modulation", "censor_augmentation", "frequency_perturbation"]
 
 # Import the amplitude modulation function
 from .amplitude_modulation import amplitude_modulation
+
+# Import the censoring augmentation function
+from .censor_augmentation import censor_augmentation
+
+# Import the frequency perturbation function
+from .frequency_perturbation import frequency_perturbation
